64a21344678f94dbe06d7986d88191a21afc7099
Tressos-Aristomenis <34586179+Tressos-Aristomenis@users.noreply.github.com>
2018-08-20 17:32:42 +0300

    Update PlayerType.java

--- enums/PlayerType.java
+++ enums/PlayerType.java
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 package enums;
 
 public enum PlayerType {
 	HUMAN, COMPUTER;
-=======
-package enums;
-
-public enum PlayerType {
-	HUMAN, COMPUTER;
->>>>>>> fe54c078370d5a5dd5db14c8b5e686f72cb18008
 }
