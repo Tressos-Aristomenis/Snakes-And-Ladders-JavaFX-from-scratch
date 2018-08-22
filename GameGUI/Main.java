fb5f6d3e7af7a83e699522a9d931c4bb748cec19
Aristomenis Tressos <ninjacawboyzz@gmail.com>
2018-08-23 01:40:44 +0300

    new commits

--- GameGUI/Main.java
+++ GameGUI/Main.java
@@ -30,42 +30,4 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-<<<<<<< HEAD
 }
-=======
-}
-
-package GameGUI;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-/**
- *
- * @author Aris
- */
-public class Main extends Application {
-	private final String FXML_LOCATION = "SnakesAndLadders.fxml";
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource(FXML_LOCATION));
-		
-		Scene scene = new Scene(root);
-		
-		stage.setScene(scene);
-		stage.setResizable(false);
-		stage.setTitle("SNAKES AND LADDERS");
-		stage.getIcons().add(new Image("icons/snake.png"));
-		stage.show();
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
->>>>>>> 22f06dd8927291c79915ec5fe5abd8b0bcea84a6
